c62fa29dbd780d839600b15aa29f551d1bbac99c
recoh <recohcity@gmail.com>
2025-06-02 22:50:18 +0800

    在布局中添加 suppressHydrationWarning 属性以解决水合警告

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <ThemeProvider attribute="class" forcedTheme="dark" disableTransitionOnChange>
           {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange> */}
           {children}
